41f77eeae38651d36afcd11fbd37420d3b6f70c8
noobuy <nobilitypig@gmail.com>
2022-05-06 13:34:39 +0900

    add pythoni

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
-print('hello world')
+print('hello')
+
+for i in range(1,20 +1) :
+    if i %15 == 0 :
+        print("yeardream")
+    elif i %3 == 0 : print("year")
+
+    elif i %5 == 0 : print("dream")
+
+    else : print(i)
+
+
 
 
